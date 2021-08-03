74fdf2c87b73deca1466e4288d1b47257792a2f5
Niamat Zawad <zawad1879@gmail.com>
2021-08-03 04:18:18 -0500

    Add rough user privileges for edit blog

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
+import { useState } from 'react';
 import Navbar from './components/navbar';
 import MobileNavbar from './components/mobile-navbar';
-import Header from './components/header';
 import About from './pages/About';
 import Skills from './pages/Skills';
 import Experience from './pages/Experience';
 import Projects from './pages/Projects';
 import Blog from './pages/Blog';
+import EditBlog from './pages/EditBlog';
+import Login from './pages/login';
+import { UserContext } from './context/UserContext';
 import { motion, AnimatePresence } from "framer-motion";
 import { Switch, Route, Redirect, useLocation, useHistory } from "react-router-dom";
-
 function App() {
   const location = useLocation();
-
+  const [token, setToken] = useState();
+  const [user, setUser] = useState('Default user');
+  console.log(user);
   return (
       <motion.div           
         initial={{ opacity: 0, y: -50 }}
@@ -24,14 +28,21 @@ function App() {
           <div class="flex-1 p-10 mt-32">
             <AnimatePresence exitBeforeEnter>
               <motion.div exit={{ opacity: 0 }}>
-                <Switch location={location} key={location.pathname}>
-                  <Route path="/about"><About/></Route>
-                  <Route path="/skills"> <Skills /> </Route>
-                  <Route path="/experience"> <Experience /> </Route>
-                  <Route path="/projects"> <Projects /> </Route>
-                  <Route path="/blog"> <Blog /> </Route>
-                  <Route path="/"> <Redirect to="/about" /> </Route>
-                </Switch>
+                <UserContext.Provider value={[user, setUser]}>
+                  <Switch location={location} key={location.pathname}>
+                    <Route path="/about"><About/></Route>
+                    <Route path="/skills"> <Skills /> </Route>
+                    <Route path="/experience"> <Experience /> </Route>
+                    <Route path="/projects"> <Projects /> </Route>
+                    <Route path="/blog"> <Blog /> </Route>
+                    <Route path="/login"> <Login /> </Route>
+                    { user != 'Default user' ? 
+                    <Route path="/edit-blog"> <EditBlog /></Route> 
+                    : <Redirect to="/about" /> 
+                    }
+                    <Route path="/"> <Redirect to="/about" /> </Route>
+                  </Switch>
+                </UserContext.Provider>
               </motion.div> 
             </ AnimatePresence>
           </div>
