9c5b0cd68e9c09e76de5c4089a69c04c2248055f
Niamat Zawad <zawad1879@gmail.com>
2021-11-04 11:08:16 -0500

    Fix redirect to landing page issue

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,20 +1,43 @@
-import { motion } from 'framer-motion';
+import { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 
 export default function LandingPage() {
-    let statement = "I am a ";
-    let role = "";
     var bgImage = {
         width: "100%",
         height: "100%",
         backgroundImage: "url(/images/wallpapers/tokyo.jpeg)",
       };
-      const arr = ["I am a software developer", "I am "]
-      
+
+    const el = useRef(null);
+    const typed = useRef(null);
+
+    useEffect(() => {
+        const options = {
+            strings: [
+            'an <i>engineer</i>',
+            'a <i>software developer</i>',
+            'a <i>bibliophile</i>',
+            'a <i>meditator</i>',
+            'a <i>traveller</i>',
+          ],
+          typeSpeed: 130,
+          backSpeed: 120,
+          loop: true
+        };
+
+        typed.current = new Typed(el.current, options);
+
+        return () => {
+            typed.current.destroy();
+        }
+          
+      }, []);
+
     return (
                 
             <div className="md:bg-cover flex justify-center items-center md:text-8xl sm:text-xl" style={ bgImage }>
                 <div>
-                    <p className="text-white">Hi</p>
+                    <p className="text-white text-7xl">I am <span style={{ whiteSpace: 'pre' }} ref={el} /></p>
                 </div>
             </div>
     );
