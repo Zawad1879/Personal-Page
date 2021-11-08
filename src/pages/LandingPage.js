63c4bdf1e24929755bd0e85a732a94a0cfeab533
Niamat Zawad <zawad1879@gmail.com>
2021-11-08 13:10:23 -0600

    Modify landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -9,9 +9,22 @@ export default function LandingPage() {
       };
 
     const el = useRef(null);
+    const topTextEl = useRef(null);
     const typed = useRef(null);
-
+    const topTextTyped = useRef(null);
     useEffect(() => {
+
+        const topTextoptions = {
+          strings: [
+          'Hi! I\'m Niamat Zawad'
+        ],
+          typeSpeed: 100,
+          backSpeed: 120,
+          showCursor: false
+        };
+        
+        topTextTyped.current = new Typed(topTextEl.current, topTextoptions);
+
         const options = {
             strings: [
             'an <i>engineer</i>',
@@ -20,7 +33,7 @@ export default function LandingPage() {
             'a <i>meditator</i>',
             'a <i>traveller</i>',
           ],
-          typeSpeed: 130,
+          typeSpeed: 180,
           backSpeed: 120,
           loop: true
         };
@@ -37,7 +50,7 @@ export default function LandingPage() {
                 
             <div className="md:bg-cover flex justify-center items-center md:text-8xl sm:text-xl" style={ bgImage }>
                 <div>
-                    <p className="text-white text-7xl">Hello I'm <span className="text-yellow-400" >Niamat Zawad</span></p>
+                    <p className="text-white text-7xl"><span className="text-yellow-400" style={{ whiteSpace: 'pre' }} ref={topTextEl} ></span></p>
                     <p className="text-white text-7xl">I am <span style={{ whiteSpace: 'pre' }} ref={el} /></p>
                 </div>
             </div>
